fc316428173a7fa0c6225f71e74fb44d5d91d9e7
Nika Bazadze <nikabazadze@gmail.com>
2023-05-12 10:46:09 +0400

    add child components and styling in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 import React from 'react';
 
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
 function App() {
   return (
-    <h1>Jammming</h1>
+    <div>
+      <header>
+        <h1>Jammming</h1>
+      </header>
+
+      <main className='app'>
+        <SearchBar />
+        <section className='main-container'>
+          <SearchResults />
+          <Playlist />
+        </section>
+      </main>
+    </div>
   );
 }
 
