d5d8e9b7dfde491efd172e7c3a3a9889a84f9714
Nika Bazadze <nikabazadze@gmail.com>
2023-05-13 14:59:11 +0400

    Obtain a Spotify access token

---
+++ src/Util/Spotify.js
@@ -0,0 +1,90 @@
+const clientId = '71324e13da2c47f194518d9617c581e1';
+const redirectUri = 'http://localhost:3000/';
+let accessToken;
+
+function generateRandomString(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+    }
+  
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+  
+    return base64encode(digest);
+}
+
+function requestAuthorization() {
+    let codeVerifier = generateRandomString(128);
+
+    generateCodeChallenge(codeVerifier).then(codeChallenge => {
+        let state = generateRandomString(16);
+        let scope = 'playlist-modify-public';
+        localStorage.setItem('code_verifier', codeVerifier);
+        let args = new URLSearchParams({
+            response_type: 'code',
+            client_id: clientId,
+            scope: scope,
+            redirect_uri: redirectUri,
+            state: state,
+            code_challenge_method: 'S256',
+            code_challenge: codeChallenge
+        });
+        window.location = 'https://accounts.spotify.com/authorize?' + args;
+    });
+}
+
+function requestAccessToken() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let code = urlParams.get('code');
+    let codeVerifier = localStorage.getItem('code_verifier');
+    let body = new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: redirectUri,
+        client_id: clientId,
+        code_verifier: codeVerifier
+    });
+
+    fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('HTTP status ' + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        localStorage.setItem('access_token', data.access_token);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+const Spotify = {
+    getAccessToken() {
+        requestAuthorization();
+        requestAccessToken();
+    }
+}
+
+export default Spotify;
